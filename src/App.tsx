74e82293f433984e6426e9fe03a37cc8a7c15cbd
tristhaus <tristhaus@yahoo.de>
2022-11-20 15:44:20 +0100

    Add plotly example, make some config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import Plot from 'react-plotly.js';
 
-function App() {
+const App = () => {
   return (
     <div>
-      here be content
+      <Plot
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: { color: 'red' },
+          }
+        ]}
+
+        layout={{
+          width: 500,
+          height: 500,
+          title: 'A Fancy Plot',
+          xaxis: {
+            range: [-10, 10]
+          },
+          yaxis: {
+            range: [-10, 10]
+          },
+          modebar: {
+            remove: ['autoScale2d', 'lasso2d', 'pan2d', 'select2d', 'zoom2d', 'zoomIn2d', 'zoomOut2d'],
+          }
+        }}
+      />
     </div>
   );
 }
