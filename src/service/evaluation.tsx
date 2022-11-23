072a6e1d35674c4358b1f63bb17f2509b9d307a2
tristhaus <tristhaus@yahoo.de>
2022-11-23 23:03:55 +0100

    Add branch logic incl. tests

--- src/service/evaluation.tsx
+++ src/service/evaluation.tsx
@@ -15,26 +15,117 @@ export interface TransformedData {
   y: PlotlyDatum[];
 }
 
-// each branch must be sorted
+// the limits of the window
+const minX = -10.5;
+const maxX = 10.5;
+
+// The absolute value of y after which the point shall not be included in the resulting data.
+const Limit = 100;
+
+// Epsilon is the lower limit for increments etc.
+const Epsilon = 1e-4;
+
+// TargetDistance is the intended upper bound for x increments, which should still be above \ref Epsilon.
+const TargetDistance = 5e-3;
+
+// InitialIncrement is the initial increment for x.
+const InitialIncrement = 1e-3;
+
+// LargeIncrement is the increment to find branches.
+const LargeIncrement = 1e-2;
+
+const isValidResult = (result: number): boolean => {
+  return !isNaN(result) && isFinite(result);
+}
+
+const forwardX = (x: number): number => x;
+const backwardX = (x: number): number => -x;
+
+const squareDistance = (x1: number, y1: number, x2: number, y2: number): number => (x1 - x2) ** 2 + (y1 - y2) ** 2;
+
+const workAnInterval = (func: (x: number) => number, direction: (x: number) => number, xInCurrentInterval: number): { branch: Pair[], lastValidXinCurrentInterval: number, x: number } => {
+  const branch: Pair[] = [];
+
+  let x = xInCurrentInterval + direction(Epsilon);
+  let yOptional = func(x);
+  let xOld = xInCurrentInterval;
+
+  let y = isValidResult(yOptional) ? yOptional : 0.0;
+  let yOld = isValidResult(yOptional) ? yOptional : func(xInCurrentInterval); // func(xInCurrentInterval) must be valid
+
+  let incr = InitialIncrement;
+
+  // scan the interval until it is interrupted
+  let interrupt = false;
+  while (!interrupt) {
+    interrupt = true;
+    yOptional = func(x);
+
+    if (isValidResult(yOptional)) {
+      y = yOptional;
+      interrupt = !(minX <= x && x <= maxX && -Limit <= y && y <= Limit);
+    }
+
+    if (!interrupt) {
+      const squareDist = squareDistance(x, y, xOld, yOld);
+      if (squareDist < TargetDistance || incr < Epsilon) {
+        if (squareDist < Epsilon) {
+          incr *= 2.0;
+        }
+
+        branch.push({ x: x, y: y });
+        xOld = x;
+        yOld = y;
+        x = x + direction(incr);
+      }
+      else {
+        incr *= 0.5;
+        x = x - direction(incr);
+      }
+    }
+  }
+
+  return { branch: branch, lastValidXinCurrentInterval: xOld, x: x };
+}
+
 export const createBranches = (func: (x: number) => number): Pair[][] => {
-  const branchA: Pair[] = [];
-  const branchB: Pair[] = [];
 
-  for (let x = -10.1; x < 2; x = x + 0.01) {
-    const res = func(x);
-    if (!isNaN(res) && isFinite(res)) {
-      branchA.push({ x: x, y: res });
+  let lastXinPreviousInterval = minX;
+  let x = lastXinPreviousInterval;
+
+  const retval: Pair[][] = [];
+
+  while (x < maxX) {
+    // look for interval
+    let xInCurrentInterval = lastXinPreviousInterval;
+    let foundInterval = false;
+    while (!foundInterval && xInCurrentInterval < maxX) {
+      xInCurrentInterval += LargeIncrement;
+      const result = func(xInCurrentInterval);
+      foundInterval = isValidResult(result);
     }
+
+    // maybe the function is not defined anywhere in our window
+    if (!foundInterval) {
+      break;
+    }
+
+    // work inside interval - backward
+    const backwardResult = workAnInterval(func, backwardX, xInCurrentInterval);
+
+    // work inside interval - forward
+    const forwardResult = workAnInterval(func, forwardX, xInCurrentInterval);
+
+    // finish up interval
+    lastXinPreviousInterval = forwardResult.lastValidXinCurrentInterval;
+    x = forwardResult.x;
+
+    retval.push(backwardResult.branch.concat(forwardResult.branch).sort((a, b) => a.x - b.x))
   }
 
-  for (let x = 2.1; x < 10.1; x = x + 0.01) {
-    const res = func(x);
-    if (!isNaN(res) && isFinite(res)) {
-      branchB.push({ x: x, y: res });
-    }
-  }
+  // todo: check if we cannot have empty retval
 
-  return [branchA, branchB];
+  return retval.filter(value => value.length > 1);
 };
 
 export const transform = (data: Data): TransformedData[] => {
--- src/test/evaluation.test.tsx
+++ src/test/evaluation.test.tsx
@@ -1,13 +1,55 @@
 import { createBranches, Data, transform, TransformedData } from "../service/evaluation";
 
-test('INCORRECT LOGIC -- always two branches -- create two branches', () => {
+test('for x^2, create two branches', () => {
   const f = (x: number): number => {
-    return Math.sqrt(x);
+    return x ** 2;
+  }
+
+  const result = createBranches(f);
+
+  expect(result).toHaveLength(1);
+});
+
+test('for 1/x, create two branches', () => {
+  const f = (x: number): number => {
+    return 1.0 / x;
   }
 
   const result = createBranches(f);
 
   expect(result).toHaveLength(2);
+});
+
+test('for tan(x), create seven branches', () => {
+  const f = (x: number): number => {
+    return Math.tan(x);
+  }
+
+  const result = createBranches(f);
+
+  expect(result).toHaveLength(7);
+});
+
+test('for ln(x), create one correct branch', () => {
+  const f = (x: number): number => {
+    return Math.log(x);
+  }
+
+  const result = createBranches(f);
+
+  expect(result).toHaveLength(1);
+  expect(result[0][0].x).toBeGreaterThan(0.0)
+});
+
+test('for sqrt(-1), create no branches', () => { // todo : check if this conforms to the use case in plotly
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const f = (_x: number): number => {
+    return Math.sqrt(-1);
+  }
+
+  const result = createBranches(f);
+
+  expect(result).toHaveLength(0);
 });
 
 test('single branch transforms to one contiguous data', () => {
