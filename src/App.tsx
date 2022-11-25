ef9dd9411594544e60c68e5372dca39a37ffe0bf
tristhaus <tristhaus@yahoo.de>
2022-11-25 07:03:17 +0100

    Add fixed dots, set up colors, legend

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import Plot from 'react-plotly.js';
-import { Data as PlotlyData} from 'plotly.js';
+import { Data as PlotlyData, Shape as PlotlyShape } from 'plotly.js';
 
 import ExpressionsForm from './form/ExpressionsForm';
 import { FormValues } from './types';
 
 import { createBranches, Data, Pair, transform } from './service/evaluation'
+import { Dot, getDotsWithStatus } from './service/dot'
 
-const createData = (expressions: string[]): PlotlyData[] => {
+const createGraphData = (expressions: string[]): { plotlyData: PlotlyData[], rawData: Data } => {
 
   const evaluate = (expressions: string[]): Data => {
 
@@ -23,27 +24,69 @@ const createData = (expressions: string[]): PlotlyData[] => {
     return retval;
   };
 
-  const data = evaluate(expressions);
+  const graphData = evaluate(expressions);
 
-  const transformedDataObjects = transform(data);
+  const transformedGraphData = transform(graphData);
 
-  const decoratedDataObjects = transformedDataObjects.map<PlotlyData>(tdo => {
+  const graphColors = [
+    '#000000', // Black
+    '#0000FF', // Blue
+    '#9400D3', // DarkViolet
+    '#A52A2A', // Brown
+    '#B0E0E6'  // PowderBlue
+  ]
+
+  const decoratedGraphData = transformedGraphData.map<PlotlyData>((tdo, index) => {
     return {
       ...tdo,
       type: 'scatter',
       mode: 'lines',
-      marker: { color: 'red' },
+      name: `f ${index}`,
+      marker: { color: graphColors[index] },
       connectgaps: false,
     }
   })
 
-  return decoratedDataObjects;
+  return { plotlyData: decoratedGraphData, rawData: graphData };
+}
+
+const createDotShapes = (data: Data): Partial<PlotlyShape>[] => {
+
+  const createDotShape = (dot: Dot, color: string): Partial<PlotlyShape> => {
+    return {
+      type: 'circle',
+      xref: 'x',
+      yref: 'y',
+      x0: dot.x - dot.radius,
+      x1: dot.x + dot.radius,
+      y0: dot.y - dot.radius,
+      y1: dot.y + dot.radius,
+      opacity: 0.8,
+      fillcolor: color,
+      line: {
+        color: color,
+      },
+    }
+  }
+
+  const dots = getDotsWithStatus(data);
+
+  const retval: Partial<PlotlyShape>[] = [
+    ...dots.GoodActive.map(dot => createDotShape(dot, '#00008B')),   // DarkBlue 
+    ...dots.GoodInactive.map(dot => createDotShape(dot, '#ADD8E6')), // LightBlue
+    ...dots.BadActive.map(dot => createDotShape(dot, '#FF4500')),    // OrangeRed
+    ...dots.BadInactive.map(dot => createDotShape(dot, '#FFB6C1'))   // LightPink
+  ]
+
+  return retval;
 }
 
 const App = () => {
   const [fs, setFs] = useState<string[]>(["Math.sqrt(x)"]);
 
-  const data = createData(fs);
+  const { plotlyData, rawData } = createGraphData(fs);
+
+  const shapes = createDotShapes(rawData);
 
   const plot = (values: FormValues) => {
     setFs(values.expressions);
@@ -52,7 +95,7 @@ const App = () => {
   return (
     <div>
       <Plot
-        data={data}
+        data={plotlyData}
         layout={{
           width: 500,
           height: 500,
@@ -65,7 +108,8 @@ const App = () => {
           },
           modebar: {
             remove: ['autoScale2d', 'lasso2d', 'pan2d', 'select2d', 'zoom2d', 'zoomIn2d', 'zoomOut2d'],
-          }
+          },
+          shapes: shapes,
         }}
       />
       <ExpressionsForm onSubmit={plot} />
---
+++ src/service/dot.tsx
@@ -0,0 +1,47 @@
+import { Pair, Data } from './evaluation';
+
+export interface Dot extends Pair {
+  radius: number;
+}
+
+interface Dots {
+  GoodActive: Dot[];
+  GoodInactive: Dot[];
+  BadActive: Dot[];
+  BadInactive: Dot[];
+}
+
+const defaultRadius = 0.25;
+
+export const getDotsWithStatus = (data: Data): Dots => {
+  return {
+    GoodActive: [
+      { x: 1.0, y: 1.0, radius: defaultRadius },
+      { x: 1.0, y: (1.0 + 1.0), radius: defaultRadius },
+      { x: 1.0, y: (1.0 + 2.0), radius: defaultRadius },
+      { x: 1.0, y: (1.0 + 3.0), radius: defaultRadius },
+      { x: 1.0, y: (1.0 + 4.0), radius: defaultRadius },
+    ],
+    GoodInactive: [
+      { x: 2.0, y: 1.41, radius: defaultRadius },
+      { x: 2.0, y: (1.41 + 1.0), radius: defaultRadius },
+      { x: 2.0, y: (1.41 + 2.0), radius: defaultRadius },
+      { x: 2.0, y: (1.41 + 3.0), radius: defaultRadius },
+      { x: 2.0, y: (1.41 + 4.0), radius: defaultRadius },
+    ],
+    BadActive: [
+      { x: 3.0, y: 1.73, radius: defaultRadius },
+      { x: 3.0, y: (1.73 + 1.0), radius: defaultRadius },
+      { x: 3.0, y: (1.73 + 2.0), radius: defaultRadius },
+      { x: 3.0, y: (1.73 + 3.0), radius: defaultRadius },
+      { x: 3.0, y: (1.73 + 4.0), radius: defaultRadius },
+    ],
+    BadInactive: [
+      { x: 4.0, y: 2.0, radius: defaultRadius },
+      { x: 4.0, y: (2.0 + 1.0), radius: defaultRadius },
+      { x: 4.0, y: (2.0 + 2.0), radius: defaultRadius },
+      { x: 4.0, y: (2.0 + 3.0), radius: defaultRadius },
+      { x: 4.0, y: (2.0 + 4.0), radius: defaultRadius },
+    ]
+  };
+}
